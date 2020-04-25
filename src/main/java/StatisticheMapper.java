366061ae3e49037e53b0065701e6f8b20ac92767
luca <lucacarloni2@gmail.com>
2020-04-25 15:58:08 +0200

    First commit

--- src/main/java/StatisticheMapper.java
+++ src/main/java/StatisticheMapper.java
@@ -9,6 +9,7 @@ public class StatisticheMapper extends Mapper<Object, Text, Text, IntWritable> {
 	@Override
 	protected void map(Object key, Text value, Mapper<Object, Text, Text, IntWritable>.Context context)
 			throws IOException, InterruptedException {
+		  
 		
 		
 		
