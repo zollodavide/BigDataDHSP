a84fb359287e3a03d17e9720e8971a4aa4787e2c
ubuntu <you@example.com>
2020-05-06 18:05:42 +0200

    Aggiustati ultimi errori e testati i risultati.

--- src/main/java/mapreduce/StatisticheReducer.java
+++ src/main/java/mapreduce/StatisticheReducer.java
@@ -23,39 +23,38 @@ public class StatisticheReducer extends Reducer<Text, StockPricesCustomValue, Te
 		
 		double firstPrice2008 = 0;
 		double lastPrice2018 = 0;
-		int minMese2008=0, minGiorno2008=0, maxMese2018=0, maxGiorno2018=0;
+		int minMese2008=12, minGiorno2008=28, maxMese2018=1, maxGiorno2018=1, minAnno2008=3000, maxAnno2018=1000;
 		
 		double max = Double.MIN_VALUE;
 		double min = Double.MAX_VALUE;
 		int count = 0;
+		int a=0,b=0;
 		long sum = 0;
 		
 		for (StockPricesCustomValue stock : value) {
-						
-			if(stock.getAnno() == 2008) {
-				if(StatisticheUtility.dataMinore(stock.getMese(),stock.getGiorno(), minMese2008, minGiorno2008)) {
-					minMese2008 = stock.getMese();
-					minGiorno2008 = stock.getGiorno();
-					firstPrice2008 = stock.getPrezzoChiusura();
-				}
+
+			if(StatisticheUtility.dataMinore(stock.getAnno(),stock.getMese(),stock.getGiorno(),minAnno2008, minMese2008, minGiorno2008)) {
+				minAnno2008 = stock.getAnno();
+				minMese2008 = stock.getMese();
+				minGiorno2008 = stock.getGiorno();
+				firstPrice2008 = stock.getPrezzoChiusura();
 			}
 			
-			if(stock.getAnno() == 2018) {
-				if(StatisticheUtility.dataMaggiore(stock.getMese(),stock.getGiorno(), maxMese2018, maxGiorno2018)) {
-					maxMese2018 = stock.getMese();
-					maxGiorno2018 = stock.getGiorno();
-					lastPrice2018 = stock.getPrezzoChiusura();
-				}
+			if(StatisticheUtility.dataMaggiore(stock.getAnno(), stock.getMese(),stock.getGiorno(), maxAnno2018,maxMese2018, maxGiorno2018)) {
+				maxAnno2018 = stock.getAnno();
+				maxMese2018 = stock.getMese();
+				maxGiorno2018 = stock.getGiorno();
+				lastPrice2018 = stock.getPrezzoChiusura();
 			}
 				
 			min = Math.min(min, stock.getMinPrezzo());
 			max = Math.max(max, stock.getMaxPrezzo());
 			sum += stock.getVolume();
 			count++;
+			
 		}
 		
-		double variazionePercentuale = Math.round(((firstPrice2008 - lastPrice2018) / lastPrice2018)*100);
-		//QUANDO IL NUMERO È A UNA CIFRA SBAGLIA
+		double variazionePercentuale = Math.round(((lastPrice2018 - firstPrice2008) / firstPrice2008)*100);
 		
 		double mean = sum/count;
 	
